dcf00e3354fe2e5ded6bf70e22f6f862fef9e313
Anthony Shew <anthonyshew@gmail.com>
2020-02-12 19:56:49 -0700

    Create documentation for SlidePanel.

--- src/components/SlidePanel/documentation.js
+++ src/components/SlidePanel/documentation.js
@@ -1,3 +1,25 @@
-const docs = `Coming soon...`
+const docs = `
+##### Purpose\n
+A section for a space of short description with an accompanying image.\n
+---
+##### Features\n
+Works with svg or image.\n
+Optional link formatted as a button.\n
+---
+##### Props\n
+mirror: Boolean. Flips the order of the halves of the view.\n
+heading: String. Creates a header for the section.\n
+image: URL string. Image content.\n
+imageAlt: String. Alt tag for image.\n
+svg: Component. SVG to use instead of image.\n
+text: String. Description content for section.\n
+backgroundColor: String. CSS property to set background color for entire section.\n
+color: String. CSS property to set color of description text.\n
+button: Boolean. Places a link button into the section.\n
+buttonLink: URL string. Where to send client.\n
+buttonText: String. Text for link button.\n
+buttonColor: String. CSS property for text color of link button.\n
+buttonBackgroundColor: String. CSS property to set background color of button.\n
+`
 
 export default docs
